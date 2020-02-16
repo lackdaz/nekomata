95888e6c71f3cceefcdbe5d55ac46a42c3a89bd3
Seth Loh <lackdaz@gmail.com>
2020-02-17 02:41:26 +0800

    working code, some bugs but working

--- include/pin_definitions.h
+++ include/pin_definitions.h
@@ -15,23 +15,13 @@
     #define CLOCK_PIN -1
     #define TOUCH_PIN 27
   #elif ESP8266
-    #define RX_PIN RX
-    #define TX_PIN TX
-
-    #define CLOCK_PIN D4
-    #define DATA_PIN D3
-    #define RST_PIN -1
-
-    #define sharpLEDPin D0
-    #define sharpVoPin A0
-    #define SHARP_GND_PIN D5
-    #define SHARP_VCC_PIN D6
   #endif
 #elif BUILD_TYPE == BUILD_TYPE_NODE
-  #ifdef ESP8266
+  #ifdef ESP32
+    #define DATA_PIN 23
     #define CLOCK_PIN -1
-    #define DATA_PIN D3
-    #define RST_PIN -1
+    #define TOUCH_PIN 27
+  #elif ESP8266
   #endif
 #elif BUILD_TYPE == BUILD_TYPE_ROOT
   #ifdef ESP32
--- src/node/main.cpp
+++ src/node/main.cpp
@@ -1,124 +1,250 @@
 
 
-//************************************************************
-// this is a simple example that uses the painlessMesh library
-//
-// 1. sends a silly message to every node on the mesh at a random time between 1 and 5 seconds
-// 2. prints anything it receives to Serial.print
-//
-//
-//************************************************************
-#include <Arduino.h>
-#include <painlessMesh.h>
-#define FASTLED_INTERRUPT_RETRY_COUNT 0
-#include <FastLED.h>
-#include <U8g2lib.h>
+  //************************************************************
+  // this is a simple example that uses the painlessMesh library
+  //
+  // 1. sends a silly message to every node on the mesh at a random time between 1 and 5 seconds
+  // 2. prints anything it receives to Serial.print
+  //
+  //
+  //************************************************************
+  #include <Arduino.h>
+  #include <painlessMesh.h>
+  #define FASTLED_INTERRUPT_RETRY_COUNT 0
+  #include <FastLED.h>
+  #include "pin_definitions.h"
 
-#define   MESH_PREFIX     "snf"
-#define   MESH_PASSWORD   "chewbacca"
-#define   MESH_PORT       5555
+  #define   MESH_PREFIX     "snf"
+  #define   MESH_PASSWORD   "chewbacca"
+  #define   MESH_PORT       5555
 
-#define NUM_LEDS 64*3
-#define NUM_SEG 64
+  #define NUM_LEDS 1
 
-#define DATA_PIN D3
-#define CLOCK_PIN -1
+  #ifdef MOCK
+    #define LED_TYPE APA106
+  #else
+    #define LED_TYPE WS2812B
+  #endif
 
-#ifdef TESTER
-  #define LED_TYPE APA106
-#else
-  #define LED_TYPE WS2812B
-#endif
+  #define DEBUG
 
-// #define BREATHE_DURATION    7000 // milliseconds until cycle repeat
+  #define INHABIT_DELAY (TASK_MILLISECOND * 2000)
 
-CRGB leds[NUM_LEDS];
+  // TODO: DO POLL
+  // TODO: CODE OUT PREASSIGNMENT
 
-Scheduler userScheduler;
-painlessMesh  mesh;
+  CRGB leds[NUM_LEDS];
 
-// User stub
-void breathe();
+  Scheduler ts;
+  painlessMesh  mesh;
 
-Task task_breathe( TASK_MILLISECOND * 100 , TASK_FOREVER, &breathe );
+  int threshold = 40;
+  bool touch_detected = false;
+  bool search_in_progress = false;
 
-void breathe() {
+  #if BUILD_TYPE == BUILD_TYPE_ROOT
+    bool inhabited = true;
+  #else
+    bool inhabited = false;
+  #endif
 
-  FastLED.setBrightness(128);
-  // fill_solid(leds, NUM_LEDS, CHSV(hue, sat, val));
+  // User stub
+  void touch();
+  void flight();
+  void inhabit();
+  void knock_knock();
+  void neko_drain();
+  void neko_imbue();
+
+  Task task_touch( TASK_MILLISECOND * 500 , TASK_FOREVER, &touch );
+  Task task_flight( TASK_IMMEDIATE , TASK_ONCE, &flight, &ts, false);
+  Task task_inhabit( TASK_IMMEDIATE , TASK_ONCE, &inhabit, &ts, false );
+  Task task_knock_knock( TASK_IMMEDIATE , TASK_ONCE, &knock_knock, &ts, false );
+
+  /* Light Effects */
+  Task task_drain( TASK_IMMEDIATE , TASK_ONCE, &neko_drain, &ts, false );
+  Task task_imbue( TASK_IMMEDIATE , TASK_ONCE, &neko_imbue, &ts, false );
+
+  void touch() {
+    Serial.print(touch_detected);
+    Serial.print("|");
+    Serial.print(inhabited);
+    Serial.print("|");
+    Serial.println(search_in_progress);
 
 
-  // fill_solid(&leds[0], NUM_SEG, CRGB::Red);
-  // fill_solid(&leds[64], NUM_SEG, CRGB::Green);
-  // fill_solid(&leds[128], NUM_SEG, CRGB::Blue);
+    if (touch_detected && inhabited && !search_in_progress){
+      touch_detected = false; // reset touch flag
+      task_knock_knock.restart();
+    } else {
+      touch_detected = false; // debounce;
+    }
+  }
 
-  fill_solid(&leds[0], NUM_SEG, CRGB::Red);
-  fill_solid(&leds[64], NUM_SEG, CRGB::Blue);
-  fill_solid(&leds[128], NUM_SEG, CRGB::DarkOrchid);
-  FastLED.show();
-  Serial.print("FPS: "); Serial.println(LEDS.getFPS());   // Display FPS in serial monitor
-}
+  void flight() {
+    #ifdef DEBUG
+      Serial.println("flight");
+    #endif
 
-// Needed for painless library
-void receivedCallback( uint32_t from, String msg ) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-  digitalWrite(LED_BUILTIN, LOW);
-}
+    task_drain.restart();
 
-void newConnectionCallback(uint32_t nodeId) {
-    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
-}
+    search_in_progress = false;
+    inhabited = false;
+  }
 
-void changedConnectionCallback() {
-  Serial.printf("Changed connections\n");
-}
+  void inhabit() {
+    inhabited = true;
+    #ifdef DEBUG
+      Serial.println(String(mesh.getNodeId()) + " is now inhabited");
+    #endif
 
-void nodeTimeAdjustedCallback(int32_t offset) {
-    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
-}
+    task_imbue.restart();
+  }
 
-void setup() {
-  Serial.begin(115200);
-  LEDS.addLeds<LED_TYPE, DATA_PIN, GRB>(leds, NUM_LEDS);
-  LEDS.setCorrection( SodiumVapor );
-  // LEDS.setMaxPowerInVoltsAndMilliamps(5, 25200); 
-  // FastLED.setTemperature(0xC4FFB0); // 0xC4FFB0
-  FastLED.setTemperature(SodiumVapor); // 0xC4FFB0
-  FastLED.setBrightness(128);
-  // FastLED.setDither( 0 );
+  void knock_knock() {
+    uint32_t nodes[100];
+    byte random_index;
+    size_t i = 0;
+    SimpleList<uint32_t> myList = mesh.getNodeList();
+    SimpleList<uint32_t>::iterator itr=myList.begin();
+    random_index = random(0, myList.size());
 
-  fill_solid(leds, NUM_LEDS, CRGB::Red);
-  FastLED.show();
-  delay(1000);
-  fill_solid(leds, NUM_LEDS, CRGB::Green);
-  FastLED.show();
-  delay(1000);
-  fill_solid(leds, NUM_LEDS, CRGB::Blue);
-  FastLED.show();
-  delay(1000);
-  FastLED.clear();
+    while (itr != myList.end()) {
+      nodes[i]=(*itr);
+      // Serial.println(*itr);
+      ++itr;
+      ++i;
+    }
+    mesh.sendSingle(nodes[random_index], "?Meow anyone at home?"); // poll
+    search_in_progress = true;
+  }
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  void neko_drain() {
+    FastLED.clear();
+    FastLED.show();
+  }
 
-//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+  void neko_imbue() {
+    fill_solid(leds, NUM_LEDS, CRGB::Red);
+    FastLED.show();
+  }
 
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
-  mesh.setContainsRoot(true);
+  /* 
+    ISR
+  */
 
-  mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  void got_touch(){
+    touch_detected = true;
+  }
 
-  userScheduler.addTask( task_breathe );
-  
-  task_breathe.enable();
-  Serial.println("================");
-  Serial.println("I AM NODE. I AM MANY!");
-  Serial.println("================");
-}
+  // Needed for painless library
+  void receivedCallback( uint32_t from, String msg ) {
+    Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
 
-void loop() {
-  mesh.update();
-}
+    char callsign = msg.charAt(0);
+    switch (callsign){
+      case '?':
+        if (!inhabited) {
+          mesh.sendSingle(from, "<Come...");
+          task_inhabit.restartDelayed(INHABIT_DELAY); // TODO: Must be exact else it would be desynced
+        } else {
+          mesh.sendSingle(from, "!Go Away");
+        }
+        break;
+      case '<': // time to flight
+        task_flight.restart();
+        break;
+      case '!': // search again
+        task_knock_knock.restart();
+        break;
+      default:
+        Serial.println("Forbidden 304");
+        break;
+    }
+  }
+
+  void newConnectionCallback(uint32_t nodeId) {
+      Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+  }
+
+  void changedConnectionCallback() {
+    Serial.printf("Changed connections\n");
+  }
+
+  void nodeTimeAdjustedCallback(int32_t offset) {
+      Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+  }
+
+  struct Button {
+    const uint8_t PIN;
+    uint32_t numberKeyPresses;
+    bool pressed;
+  };
+
+  void setup() {
+    Serial.begin(115200);
+    delay(100);
+    #ifdef MOCK
+      LEDS.addLeds<LED_TYPE, DATA_PIN, RGB>(leds, NUM_LEDS);
+    #else
+      LEDS.addLeds<LED_TYPE, DATA_PIN, GRB>(leds, NUM_LEDS);
+    #endif
+    LEDS.setCorrection( SodiumVapor );
+    // LEDS.setMaxPowerInVoltsAndMilliamps(5, 25200); 
+    // FastLED.setTemperature(0xC4FFB0); // 0xC4FFB0
+    FastLED.setTemperature(SodiumVapor); // 0xC4FFB0
+    FastLED.setBrightness(128);
+    // FastLED.setDither( 0 );
+
+    randomSeed(analogRead(0));
+
+    fill_solid(leds, NUM_LEDS, CRGB::Red);
+    FastLED.show();
+    delay(1000);
+    fill_solid(leds, NUM_LEDS, CRGB::Green);
+    FastLED.show();
+    delay(1000);
+    fill_solid(leds, NUM_LEDS, CRGB::Blue);
+    FastLED.show();
+    delay(1000);
+    FastLED.clear();
+    FastLED.show();
+
+    #if BUILD_TYPE == BUILD_TYPE_ROOT
+      fill_solid(leds, NUM_LEDS, CRGB::Red);
+      FastLED.show();
+    #endif
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    touchAttachInterrupt(TOUCH_PIN, got_touch, threshold);
+
+  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+    mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+    mesh.init( MESH_PREFIX, MESH_PASSWORD, &ts, MESH_PORT );
+    mesh.setContainsRoot(true);
+    #if BUILD_TYPE == BUILD_TYPE_ROOT
+      mesh.setRoot(true);
+    #endif
+
+    mesh.onReceive(&receivedCallback);
+    mesh.onNewConnection(&newConnectionCallback);
+    mesh.onChangedConnections(&changedConnectionCallback);
+    mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+    ts.addTask( task_touch );
+    task_touch.enable();
+    #ifdef DEBUG
+      Serial.println("================");
+      #if BUILD_TYPE == BUILD_TYPE_ROOT
+        Serial.println("I AM SOURCE NEKO. I AM ONE!");
+      #else
+        Serial.println("I AM A NEKOSHRINE. I AM MANY!");
+      #endif
+      Serial.println("================");
+    #endif    
+  }
+
+  void loop() {
+    mesh.update();
+  }
